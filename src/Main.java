253c02a020934edfa7f5d624dd2fb4db5923f089
Logan Pageler <loganpageler@Logans-MacBook-Pro.local>
2021-09-29 12:38:57 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("{");
-        for (int i = 0; i < 100; i++) {
-            double val = 1 / (1 + Math.pow(Math.E, (10 - i / 5)));
-            System.out.print(val + ", ");
-            if (i % 10 == 9) {
-                System.out.println();
-            }
+        // System.out.print("{");
+        // for (int i = 0; i < 100; i++) {
+        // double val = 1 / (1 + Math.pow(Math.E, (10 - i / 5)));
+        // System.out.print(val + ", ");
+        // if (i % 10 == 9) {
+        // System.out.println();
+        // }
 
-        }
-        System.out.print("};");
+        // }
+        // System.out.print("};");
+
     }
+
 }
--- src/Node.java
+++ src/Node.java
@@ -2,18 +2,24 @@ import java.util.Random;
 
 public class Node {
     public int value;
-    public double[] weights;
-    public Node[] previous;
+    public double[] weights, previous;
+    public double bias, shift;
 
-    public Node(Node[] previous) {
+    public Node(double[] previous) {
         Random rn = new Random();
         this.previous = previous;
+        bias = rn.nextDouble();
+        shift = rn.nextDouble();
         weights = new double[previous.length];
 
         for (int i = 0; i < previous.length; i++) {
             weights[i] = rn.nextDouble();
         }
 
+    }
+
+    public double calc_value() {
+        return Utils.sigmoid(Utils.sum(previous, weights) * bias + shift);
     }
 
 }
