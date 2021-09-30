4f53c1e4464d03e3f4430a9b93ce55fb36d0b7a7
Logan Pageler <loganpageler@ip-10-186-93-70.ec2.internal>
2021-09-30 13:07:36 -0700

    testing

--- src/Network.java
+++ src/Network.java
@@ -1,75 +1,73 @@
 public class Network {
     Node[][] layers;
-    double [][] values;
-    double [] inputs;
+    double[][] values;
+    double[] inputs;
 
-    
     public Network(int[] size) {
         layers = new Node[size.length - 1][];
         values = new double[size.length - 1][];
 
-        //Create input layer
-        layers[0] = new Node[size[1]];
+        // Create input layer
+        values[0] = new double[size[1]];
         inputs = new double[size[0]];
-        layers[0] = create_layer(inputs);
+        layers[0] = create_layer(inputs, size[1]);
+        // Create rest of layers
+        for (int i = 1; i < layers.length; i++) {
 
-        //Create rest of layers
-        for(int i = 1; i < layers.length; i++) {
-
-            layers[i] = new Node[size[i + 1]];
             values[i] = new double[size[i + 1]];
-            layers[i] = create_layer(values[i]);
+            layers[i] = create_layer(values[i - 1], size[i + 1]);
         }
     }
 
     public double[] calc_output(double[] in) {
-        for(int i = 0; i < in.length; i++) {
+        for (int i = 0; i < in.length; i++) {
+
             inputs[i] = in[i];
         }
-        
-        for(int i = 1; i < layers.length; i++) {
-            for(int j = 0; j < layers[i].length; j++) {
+
+        for (int i = 0; i < layers.length; i++) {
+            for (int j = 0; j < layers[i].length; j++) {
                 values[i][j] = layers[i][j].calc_value();
             }
         }
-        
+
         return values[values.length - 1];
     }
 
-    public void learn(double[] expected) { 
+    public void learn(double[] expected) {
         int index = layers.length - 1;
         double[] errors = new double[layers[index - 1].length];
 
-        //output layer
-        for(int i = 0; i < layers[index].length; i++) {
+        // output layer
+        for (int i = 0; i < layers[index].length; i++) {
             double[] arr = layers[index][i].learn(expected[i] - values[index][i]);
-            add_errors(errors, arr);       
+            add_errors(errors, arr);
         }
 
-        //hidden layers
-        for(int i = layers.length - 2; i > 0; i--) {
-            double[] new_errors = new double[layers[i-1].length];
-            for(int j = 0; j < layers[i].length; j++) {
+        // hidden layers
+        for (int i = layers.length - 2; i > 0; i--) {
+            double[] new_errors = new double[layers[i - 1].length];
+            for (int j = 0; j < layers[i].length; j++) {
                 double[] arr = layers[i][j].learn(errors[j]);
                 add_errors(new_errors, arr);
             }
             errors = new_errors;
         }
 
-        //1st layer
-        for(int i = 0; i < layers[0].length; i++) {
-            layers[0][i].learn(errors[i]);      
+        // 1st layer
+        for (int i = 0; i < layers[0].length; i++) {
+            layers[0][i].learn(errors[i]);
         }
     }
 
-    private void add_errors(double[] errors, double[] arr){ 
-        for(int j = 0; j < arr.length; j++)
+    private void add_errors(double[] errors, double[] arr) {
+        for (int j = 0; j < arr.length; j++)
             errors[j] += arr[j];
     }
 
-    public Node[] create_layer(double[] values) {
-        Node[] layer = new Node[values.length];
-        for(int j = 0; j < values.length; j++) {
+    public Node[] create_layer(double[] values, int size) {
+        Node[] layer = new Node[size];
+        for (int j = 0; j < size; j++) {
             layer[j] = new Node(values);
         }
         return layer;
@@ -77,9 +75,9 @@ public class Network {
 
     public String toString() {
         String output = "";
-        for(Node[] layer : layers) {
+        for (Node[] layer : layers) {
             output += "[";
-            for(Node node : layer) {
+            for (Node node : layer) {
                 output += node + ", ";
             }
             output += "]\n";
--- src/Node.java
+++ src/Node.java
@@ -7,51 +7,55 @@ public class Node {
     public double[] weights, previous;
     public double bias, value;
 
-    //Previous is the values of previous nodes and the array should be kept alive elsewear
+    // Previous is the values of previous nodes and the array should be kept alive
+    // elsewear
     public Node(double[] previous) {
         Random rn = new Random();
         this.previous = previous;
-        bias = rn.nextDouble();
+        bias = rn.nextDouble() - 0.5;
         weights = new double[previous.length];
 
         for (int i = 0; i < previous.length; i++) {
-            weights[i] = rn.nextDouble();
+            weights[i] = rn.nextDouble() - 0.5;
         }
 
     }
 
-    //Returns value of node
+    // Returns value of node
     public double calc_value() {
+
         value = Utils.sigmoid(Utils.sum(previous, weights) + bias);
         return value;
     }
 
-    //Returns errors for previous row of nodes
+    // Returns errors for previous row of nodes
     public double[] calc_prev_errors(double delta) {
+
         double[] errors = new double[weights.length];
-        for(int i = 0; i < errors.length; i++) {
+        for (int i = 0; i < errors.length; i++) {
             errors[i] = weights[i] * delta;
         }
         return errors;
     }
 
-    //Returns errors for previous row of nodes and adjusts weights
+    // Returns errors for previous row of nodes and adjusts weights
     public double[] learn(double error) {
         double delta = error * Utils.transfer_derivative(value);
 
-        for(int i = 0; i < weights.length; i++) {
+        for (int i = 0; i < weights.length; i++) {
             weights[i] += learing_rate * delta * previous[i];
         }
+
         bias += learing_rate * delta;
 
         return calc_prev_errors(delta);
-        
+
     }
 
     public void learn_base(double error) {
         double delta = error * Utils.transfer_derivative(value);
 
-        for(int i = 0; i < weights.length; i++) {
+        for (int i = 0; i < weights.length; i++) {
             weights[i] += learing_rate * delta * previous[i];
         }
         bias += learing_rate * delta;
@@ -59,13 +63,11 @@ public class Node {
 
     public String toString() {
         String output = "{'value': " + value + ", 'weights': [";
-        for(double weight : weights) {
+        for (double weight : weights) {
             output += weight + ", ";
         }
         output += bias + "]}";
         return output;
     }
-
-    
 
 }
